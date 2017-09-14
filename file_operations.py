7a4129b19a8df485c101fdb4fcc27dd4b0341e2e
Vojta Oupicky <voj.oup@gmail.com>
2017-09-14 21:52:01 +0200

    Final version

--- file_operations.py
+++ file_operations.py
@@ -3,20 +3,18 @@ class FileMaster():
     def __init__(self, filepath):
         self.filepath = filepath
     def extension(self):
-        return self.filepath[-3:]
+        return self.filepath.split('.')[-1]
     def filename(self):
         parts = self.filepath.split('/')
         return parts[-1].split('.')[0]
-    # def dirpath(self):
-    #     #Your code here
+    def dirpath(self):
+        d = len(self.filename()) + len(self.extension()) + 1
+        return self.filepath[:len(self.filepath) - d]
 
 master = FileMaster('/Users/person1/Pictures/house.png')
-
 master.extension()
 # should return >>> png
-
 master.filename()
 # should return >>> house
-
-# master.dirpath()
+master.dirpath()
 # should return >>> /Users/person1/Pictures/
